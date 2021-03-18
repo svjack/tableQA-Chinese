dc3a6351446e3806916cabeb77c5b90bb69e9a18
svjack <ehangzhou@outlook.com>
2021-03-18 14:44:23 +0800

    Add files via upload

---
+++ snorkel_install.sh
@@ -0,0 +1,13 @@
+pip install "dask[dataframe]"
+pip install "dask[distributed]"
+
+pip install jieba
+pip install seaborn
+pip install sqlite_utils
+pip install sqlitefts
+pip install icecream
+
+#pip install scikit-learn==0.21.3
+#pip install keybert
+#pip install bertopic
+pip install snorkel
---
+++ topic_install.sh
@@ -0,0 +1,17 @@
+#pip install "dask[dataframe]"
+#pip install "dask[distributed]"
+
+pip install torch==1.6.0
+
+pip install jieba
+pip install seaborn
+pip install sqlite_utils
+pip install sqlitefts
+pip install icecream
+
+#pip install scikit-learn==0.21.3
+pip install protobuf
+pip install keybert
+pip install bertopic
+#pip install snorkel
+
